3547115b8f85309356fad0b6e63a498975414248
Enzo Antônio Ferreira de Araújo - SENAI <enzo.a.araujo6@aluno.senai.br>
2025-04-02 12:40:00 -0300

    criação Goku.py
    Criei um print onde se encontram as falas de Goku e Freeza em certo momento da Saga de Namekusei.

---
+++ goku.py
@@ -0,0 +1,3 @@
+print("Goku Super Saiyan: You can destroy planets, but you can never destroy what I am, friend.")
+print("Freeza: What- what are you?!")
+print("Goku Super Saiyan: I am the hope of the universe, I am the answer for all living things that cry out for peace, I am the protector of the inoccent, I am the light in the darkness, I am truth!!")
